5abdd676f3200bd98f90ba6a61c3b2ccf3eb6333
adtrae <h13065162718@163.com>
2024-01-27 21:08:42 +0800

    package

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 /**自定义全局插件名*/
-export const AppName = 'btc-memz-plugin';
+export const AppName = 'Fanji-plugin';
 
 // 导出ThePath常量，值为当前文件所在的路径
 export const ThePath = `${path.resolve().replace(/\\/g, '/')}`;
