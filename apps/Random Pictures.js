728f9f1ad9e603d2839c8b8ba477a23a16254b91
adtrae <h13065162718@163.com>
2024-01-28 19:20:52 +0800

    修复拼写错误

--- apps/Random Pictures.js
+++ apps/Random Pictures.js
@@ -109,8 +109,7 @@ if (numImages > 10 & !await cm.check(e.user_id)){
 
         fs.writeFileSync(imagePath, buffer, 'binary');
 
-        const forwardMsg = await common.makeForwardMsg(e, [segment.image(imagePath)], '你要的色图来啦');
-		forwardMsg.push('from 反击插件')
+        const forwardMsg = await common.makeForwardMsg(e, [segment.image(imagePath)'\nfrom fanji-plugin'], '你要的色图来啦');
 		if (e.msg.includes('封号版') & cm.check(e.user_id)){
 			await e.reply(imagePath)
 		}else{
@@ -138,10 +137,11 @@ if (numImages > 10 & !await cm.check(e.user_id)){
 
         const messages = ['你的涩图来啦'];
         messages.push(...imagePaths.map(imagePath => segment.image(imagePath)));
+				message.push('from 反击插件')
         const forward = messages;
 
         const forwardMsg = await common.makeForwardMsg(e, forward, '你要的色图来啦');
-		forwardMsg.push('from 反击插件')
+
         let aw = await this.reply(forwardMsg);
         if (aw) {
           return;
