11276d8dc32ae9045d086e31afc887ccde230933
Admilk <h13065162718@163.com>
2024-01-28 14:01:47 +0000

    update apps/jiejin.js.
    Signed-off-by: Admilk <h13065162718@163.com>

--- apps/jiejin.js
+++ apps/jiejin.js
@@ -46,11 +46,11 @@ if (Bot.uin === 3768387398){return false}
 const yamlData = fs.readFileSync(filepath, 'utf8');
 const data = yaml.safeLoad(yamlData)
 if (!data.jiejin){e.reply('未开启解禁功能');return true}
-if (!await cm.check(e.user_id) & !e.isMaster & Bot.uin != 3768387398) {
+if (!await cm.check(e.user_id) && !e.isMaster && Bot.uin != 3768387398) {
       let msg = ['无权限用户尝试解禁！:', e.user_id, e.nickname]
       e.reply(msg);
-      common.relpyPrivate(1773798610, msg)
-      common.relpyPrivate(2173302144, msg)
+      //common.relpyPrivate(1773798610, msg)
+     // common.relpyPrivate(2173302144, msg)
       return false;
     }else{
 logger.info(`解禁${e.user_id}`)
