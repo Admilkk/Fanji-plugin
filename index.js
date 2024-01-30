c2fb68706ab68d692029edf3b39d929b90d0a426
adtrae <h13065162718@163.com>
2024-01-30 14:16:02 +0800

    修复字体

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function removeBlackQQ() {
 
 const watcher = chokidar.watch(configFilePath);
 watcher.on('change', async () => {
-  logger.info('[修改配置文件]other.yaml.');
+  logger.mark('[修改配置文件][other]');
   await removeBlackQQ();
 });
 
