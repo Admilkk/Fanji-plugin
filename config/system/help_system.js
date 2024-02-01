fa8a2ba6179c9bce1f14aaa577afe88b1887bd9f
Admilk <h13065162718@163.com>
2024-02-01 17:08:16 +0800

    移除不必要文件

--- config/system/help_system.js
+++ config/system/help_system.js
@@ -63,6 +63,11 @@ export const helpList = [
         title: '#随机furry图',
         desc: '字面意思'
       },
+	  {
+        icon: 29,
+        title: '#随机白丝',
+        desc: '字面意思'
+      },
       {
         icon: 29,
         title: '#随机原神',
@@ -74,6 +79,11 @@ export const helpList = [
     group: '管理员命令',
     list: [
       {
+        icon: 29,
+        title: '#开启反击',
+        desc: '字面意思'
+      },
+	  {
         icon: 29,
         title: '#开启反击',
         desc: '字面意思'
