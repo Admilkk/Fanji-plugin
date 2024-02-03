3977ff9987b605e6ce223244ff1395ce926d7de5
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-02-03 19:05:26 +0800

    a

---
+++ apps/Random bq.js
@@ -0,0 +1,63 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+import cm from '../lib/common/CM.js';
+import common from '../lib/common/common.js';
+import { fileURLToPath } from 'url';
+import https from 'https';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+async function getEmojiNames() {
+  try {
+    const response = await fetch('https://api.yunxiyuanyxy.xyz/emoji/?list=all');
+    const data = await response.json();
+    return Object.keys(data);
+  } catch (error) {
+    console.error('Error fetching emoji data:', error.message);
+    return [];
+  }
+}
+
+// 构建正则表达式字符串
+async function buildRegexString() {
+  const emojiNames = await getEmojiNames();
+  return `/#随机\\((${emojiNames.join('|')})\\)表情/i`;
+}
+
+// 主函数
+async function main() {
+  // 获取和构建正则表达式字符串
+  const regexString = await buildRegexString();
+
+  // 输出 regexString 字符串
+  console.log(regexString);
+
+  // 直接返回正则表达式字符串
+  return regexString;
+}
+
+// 调用主函数并将返回值赋给 regs 变量
+const regs = main();
+export class apisetu extends plugin {
+  constructor() {
+    super({
+      name: '反击',
+      dsc: '反击!!!!',
+      event: 'message',
+      priority: -9999999999999999999999999999999999999999999999991,
+      rule: [
+        {
+          reg: regs, // 无r18.所以不套转发
+          fnc: 'bq',
+        }
+      ],
+    });
+  }
+async bq (e) {
+	await e.reply(regs)
+	await e.reply('aw')
+}
+
+}
