6302843830b94c1c4b627adc48f115033af559e2
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-02-03 19:22:03 +0800

    aw

--- apps/Random bq.js
+++ apps/Random bq.js
@@ -1,15 +1,18 @@
-import fetch from 'node-fetch';
-import fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml';
-import cm from '../lib/common/CM.js';
-import common from '../lib/common/common.js';
-import { fileURLToPath } from 'url';
-import https from 'https';
-import { dirname } from 'path';
+async function getEmojiNames() {
+  try {
+    const response = await fetch('https://api.yunxiyuanyxy.xyz/emoji/?list=all');
+    const data = await response.json();
+    return Object.keys(data);
+  } catch (error) {
+    console.error('Error fetching emoji data:', error.message);
+    return [];
+  }
+}
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+async function buildRegexString() {
+  const emojiNames = await getEmojiNames();
+  return `/#随机\\((${emojiNames.join('|')})\\)表情/i`;
+}
 
 export class apisetu extends plugin {
   constructor() {
@@ -20,8 +23,6 @@ export class apisetu extends plugin {
       priority: -9999999999999999999999999999999999999999999999991,
       rule: [],
     });
-
-    // Call the async initialize function after constructing the instance
     this.initialize();
   }
 
@@ -44,22 +45,4 @@ export class apisetu extends plugin {
   async bq(e) {
     await e.reply('aw');
   }
-}
-
-// 获取表情名字
-async function getEmojiNames() {
-  try {
-    const response = await fetch('https://api.yunxiyuanyxy.xyz/emoji/?list=all');
-    const data = await response.json();
-    return Object.keys(data);
-  } catch (error) {
-    console.error('Error fetching emoji data:', error.message);
-    return [];
-  }
-}
-
-// 构建正则表达式字符串
-async function buildRegexString() {
-  const emojiNames = await getEmojiNames();
-  return `/#随机\\((${emojiNames.join('|')})\\)表情/i`;
 }
