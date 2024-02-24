0ea1a5c2474981250371fd2422a610a6954fe086
Admilk <h13065162718@163.com>
2024-02-24 10:12:05 +0000

    update apps/一键禁言某人.js.
    Signed-off-by: Admilk <h13065162718@163.com>

--- apps/一键禁言某人.js
+++ apps/一键禁言某人.js
@@ -41,9 +41,9 @@ let aw = this.e.at ? this.e.at : match[2];
         }
     }
     const message = [];
-    const groupList = Array.from(await Bot.gl.values());
+    const groupList = Array.from(await Bot[e.self_id].gl.values());
     for (const group of groupList) {
-       let success = Bot.pickGroup(group.group_id).muteMember(aw, time);
+       let success = Bot[e.self_id].pickGroup(group.group_id).muteMember(aw, time);
         let is_admin = group.admin_flag
         if (is_admin) {
             message.push(`在群 ${group.group_id} 禁言 ${aw} ${time}秒成功`);
