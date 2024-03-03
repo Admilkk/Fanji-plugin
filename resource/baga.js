41ab029dbd659753193f23168d4a3d888328f9a4
xwy <gudao615970148@163.com>
2024-03-03 14:22:21 +0000

    000000000
    Signed-off-by: xwy <gudao615970148@163.com>

--- resource/baga.js
+++ resource/baga.js
@@ -2,7 +2,6 @@ import fetch from "node-fetch";
 import plugin from '../../../lib/plugins/plugin.js';
 import fs from 'fs'
 import YAML from 'yaml'
-
 import { json } from "sequelize";
 
 export class baga extends plugin {
@@ -21,10 +20,12 @@ export class baga extends plugin {
   }
   async baga(e) {
    // console.log(e.msg)
-    let res = await fetch('https://gitee.com/yunxiyuan_admin/gaffe/raw/master/alias.json');
+    let res = await fetch('https://gitee.com/yunxiyuan_admin/gaffe/raw/master/alias.json');//记得提醒@唐溪 在别名里加入原名
     let obj = await res.json()
     let jsonStr = obj;
-   let ressname = checkText(e.msg, jsonStr)
+    //获取到原名别名对照json
+
+   let ressname = checkText(e.msg, jsonStr)  //通过函数获取判断后的内容（如果匹配，则返回对应原名，不匹配则返回空内容）
   // console.log(ressname)
     if(ressname !== ''){
       console.log('已匹配到'+ressname)
@@ -37,6 +38,13 @@ export class baga extends plugin {
   }
 }
 
+
+/**
+* 匹配json内容v1.0
+* @param txt 需要判断的文本
+* @param jsonObj 传入的json
+* @return key 返回匹配到的原名；如果未匹配到，则返空内容
+*/
 function checkText(txt, jsonObj) {
   for (let key in jsonObj) {
     if (Array.isArray(jsonObj[key])) {
