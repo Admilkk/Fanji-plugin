1e7c3c70d525578f367c165e38077fa21b7c3275
Admilk <h13065162718@163.com>
2024-03-04 05:19:18 +0000

    删除文件 components

--- components/index.js
+++
@@ -1,7 +0,0 @@
-const Path = process.cwd();
-const Plugin_Name = 'Fanji-plugin'
-const Plugin_Path = `${Path}/plugins/${Plugin_Name}`;
-import Version from './Version.js'
-import Data from './Data.js'
-import Common from './Common.js'
-export { Common, Data, Version, Path, Plugin_Name, Plugin_Path }
