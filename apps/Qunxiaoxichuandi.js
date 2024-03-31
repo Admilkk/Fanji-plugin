6f325cfafe93234dcb258f328cffa908bdd28a7f
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-03-31 04:10:35 +0000

    update apps/Qunxiaoxichuandi.js.
    Signed-off-by: Admilk <13205155+adrae@user.noreply.gitee.com>

--- apps/Qunxiaoxichuandi.js
+++ apps/Qunxiaoxichuandi.js
@@ -126,7 +126,7 @@ const msgid = res.find(str => {
 let isreply = typeof segment.reply === 'function'? true : false
 let wx = /chatroom/i.test(group)
 	  try{
-		  let msg = [wx ? '' :  isreply? segment.reply(id):'',segment.at(qq),`群聊${e.group_id}(${e.group_name})\n回复人:${e.member.card}(${e.user_id})\n`, wx ? e.msg:e.message, `\n\n此消息不支持引用回复`]
+		  let msg = [wx ? '' :  isreply? segment.reply(id):'',wx? '':segment.at(qq),`群聊${e.group_id}(${e.group_name})\n回复人:${e.member.card}(${e.user_id})\n`, wx ? e.msg:e.message, `\n\n此消息不支持引用回复`]
 	        Bot.pickGroup(group).sendMsg(msg/* , true, false */);
 	  		   await e.reply('✅ 已把消息发给它了哦~')
 	  }catch(error){
