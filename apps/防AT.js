71da1f13efcdd3e22be9e944abf053ddf63e3b9a
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-04-03 11:25:25 +0000

    格式化
    Signed-off-by: Admilk <13205155+adrae@user.noreply.gitee.com>

--- apps/防AT.js
+++ apps/防AT.js
@@ -5,53 +5,53 @@ export default class FanjiUNatAll extends plugin {
             name: '反击[防at全体]',
             priority: -Infinity,
             rule: [
-				{
-					reg: /^#?(开启|关闭)(本群)?防(at|召唤|艾特)?全体$/i,
-					fnc: 'q'
-				},
                 {
-					reg: /^#?设置(本群)?防(at|召唤|艾特)?全体时间\s*(\d+)?$/i,
-					fnc: 'q2'
-				},
+                    reg: /^#?(开启|关闭)(本群)?防(at|召唤|艾特)?全体$/i,
+                    fnc: 'q'
+                },
                 {
-					reg: '',
+                    reg: /^#?设置(本群)?防(at|召唤|艾特)?全体时间\s*(\d+)?$/i,
+                    fnc: 'q2'
+                },
+                {
+                    reg: '',
                     log: false,
                     fnc: 'fd',
                 }
             ],
-    });
-	}
-    async q2(e){
-                if (!(e.isMaster||await cm.check(e.user_id))) return false
+        });
+    }
+    async q2(e) {
+        if (!(e.isMaster || await cm.check(e.user_id))) return false
         let isGroup = e.msg.includes('本群')
         let time = e.msg.match(/(\d+)/)
-        if (time){
-        time = time[0]
-        }else{
+        if (time) {
+            time = time[0]
+        } else {
             time = 600
         }
-            if (isGroup){
-                await redis.set(`Fanji:AT:${this.e.group_id}:time`, time)
-                await e.reply(`本群防at时间已设置为${time}`)
-                return
-            }
-            await redis.set(`Fanji:AT:time`, time)
-            await e.reply(`全局防at时间设置为${time}`)
+        if (isGroup) {
+            await redis.set(`Fanji:AT:${this.e.group_id}:time`, time)
+            await e.reply(`本群防at时间已设置为${time}`)
+            return
         }
-    async q(e){
-          if (!(e.isMaster||await cm.check(e.user_id))) return false
+        await redis.set(`Fanji:AT:time`, time)
+        await e.reply(`全局防at时间设置为${time}`)
+    }
+    async q(e) {
+        if (!(e.isMaster || await cm.check(e.user_id))) return false
         let operate = e.msg.includes('开启')
         let isGroup = e.msg.includes('本群')
-        if (operate){
-            if (isGroup){
+        if (operate) {
+            if (isGroup) {
                 await redis.set(`Fanji:AT:kq:${this.e.group_id}`, 'true')
                 await e.reply(`开启本群防at成功`)
                 return
             }
             await redis.set(`Fanji:AT:kq`, 'true')
             await e.reply(`开启全局防at成功`)
-        }else{
-            if (isGroup){
+        } else {
+            if (isGroup) {
                 await redis.set(`Fanji:AT:kq:${this.e.group_id}`, 'false')
                 await e.reply(`关闭本群防at成功`)
                 return
@@ -61,35 +61,35 @@ export default class FanjiUNatAll extends plugin {
         }
 
     }
-  async fd(e){
-    if (e.atall){
-        // 设置默认时间
-        if (await redis.get(`Fanji:AT:time`) == null || !(await redis.get(`Fanji:AT:time`))) {
-            await redis.set(`Fanji:AT:time`, 600);
-        }
-        const globalAtEnabled = await redis.get(`Fanji:AT:kq`);
-        const groupAtEnabled = await redis.get(`Fanji:AT:kq:${this.e.group_id}`);
-        
-        if (globalAtEnabled !== 'true' && groupAtEnabled !== 'true') {
-            return false;
-        }
-        
-        // 检查权限
-        if (!e.member.is_admin && !e.member.is_owner){
-            if (await cm.check(this.e.user_id)) return false
-            if (e.group.is_admin){
-                await e.reply('检测到无权限at全体！开始禁言操作');
-                const time = await redis.get(`Fanji:AT:${this.e.group_id}:time`) || await redis.get(`Fanji:AT:time`);
-                await e.group.recallMsg(e.message_id);
-                await e.group.muteMember(e.user_id, time);
-            } else {
-                await this.reply(segment.at(this.e.group.info.owner_id));
-                await this.reply(segment.at(this.e.group.info.owner_id));
-                await this.reply([`这个坏人无权限@全体！！！`, segment.at(e.user_id)]);
-                await this.reply([`这个坏人无权限@全体！！！`, segment.at(e.user_id)]);
+    async fd(e) {
+        if (e.atall) {
+            // 设置默认时间
+            if (await redis.get(`Fanji:AT:time`) == null || !(await redis.get(`Fanji:AT:time`))) {
+                await redis.set(`Fanji:AT:time`, 600);
+            }
+            const globalAtEnabled = await redis.get(`Fanji:AT:kq`);
+            const groupAtEnabled = await redis.get(`Fanji:AT:kq:${this.e.group_id}`);
+
+            if (globalAtEnabled !== 'true' && groupAtEnabled !== 'true') {
+                return false;
+            }
+
+            // 检查权限
+            if (!e.member.is_admin && !e.member.is_owner) {
+                if (await cm.check(this.e.user_id)) return false
+                if (e.group.is_admin) {
+                    await e.reply('检测到无权限at全体！开始禁言操作');
+                    const time = await redis.get(`Fanji:AT:${this.e.group_id}:time`) || await redis.get(`Fanji:AT:time`);
+                    await e.group.recallMsg(e.message_id);
+                    await e.group.muteMember(e.user_id, time);
+                } else {
+                    await this.reply(segment.at(this.e.group.info.owner_id));
+                    await this.reply(segment.at(this.e.group.info.owner_id));
+                    await this.reply([`这个坏人无权限@全体！！！`, segment.at(e.user_id)]);
+                    await this.reply([`这个坏人无权限@全体！！！`, segment.at(e.user_id)]);
+                }
             }
         }
+        return false;
     }
-    return false;
-}
 }
