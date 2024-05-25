3484c2a6d71b0a92a1248c6adf0cb1671a6eab28
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-05-25 10:48:35 +0000

    update robot/index.js.
    Signed-off-by: Admilk <13205155+adrae@user.noreply.gitee.com>

--- robot/index.js
+++ robot/index.js
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import path from 'path';
 import { AppName } from '../app.config.js';
 import chalk from 'chalk';
+
 export const appsOut = async ({ AppsName }) => {
   // 获取插件名称
   const firstName = `plugins/${AppName}`;
@@ -40,28 +41,32 @@ export const appsOut = async ({ AppsName }) => {
   for (let item of sum) {
     // 获取插件地址
     let address = `..${item.replace(/\\/g, '/').replace(`${firstName}`, '')}`;
-    // 导入插件
-    let allExport = await import(address);
-    // 获取插件属性
-    let keys = Object.keys(allExport);
-    // 遍历插件属性
-    keys.forEach(key => {
-      // 判断插件属性是否为类
-      if (allExport[key].prototype) {
-        // 判断插件对象是否已经存在该类
-        if (apps.hasOwnProperty(key))
+    try {
+      // 导入插件
+      let allExport = await import(address);
+      // 获取插件属性
+      let keys = Object.keys(allExport);
+      // 遍历插件属性
+      keys.forEach(key => {
+        // 判断插件属性是否为类
+        if (allExport[key].prototype) {
+          // 判断插件对象是否已经存在该类
+          if (apps.hasOwnProperty(key))
+            logger.info(
+              `Template detection:已经存在class ${key}同名导出\n    ${address}`
+            );
+          // 存储插件对象
+          apps[key] = allExport[key];
+          logger.info(chalk.cyan('[Fanji-plugin]'), `加载${address.replace(/\.\.\/apps\//g, '')}文件完成`);
+        } else {
           logger.info(
-            `Template detection:已经存在class ${key}同名导出\n    ${address}`
+            `Template detection:存在非class属性${key}导出\n    ${address}`
           );
-        // 存储插件对象
-        apps[key] = allExport[key];
-        logger.info(chalk.cyan('[Fanji-plugin]'), `加载${address.replace(/\.\.\/apps\//g, '')}文件完成`);
-      } else {
-        logger.info(
-          `Template detection:存在非class属性${key}导出\n    ${address}`
-        );
-      }
-    });
+        }
+      });
+    } catch (error) {
+      logger.error(chalk.red(`[Fanji-plugin] 加载${address.replace(/\.\.\/apps\//g, '')}文件失败: ${error.message}`));
+    }
   }
   // 返回插件对象
   return apps;
