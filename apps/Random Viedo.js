3f7a2a8eaeb784fd8950321c9dc97418b0ff1e6c
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-06-21 12:15:41 +0800

    细节优化

--- apps/Random Viedo.js
+++ apps/Random Viedo.js
@@ -2,10 +2,8 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
-//import cm from '../lib/common/CM.js';
 import common from '../lib/common/common.js';
 import { fileURLToPath } from 'url';
-import https from 'https';
 import { dirname } from 'path';
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
@@ -30,7 +28,7 @@ const correspondingValues = [
   'rewu', 'chuanda', 'xuejie', 'kawayi', 'qingchun', 'hanfu'
 
 ]
-export class apiviedo extends plugin {
+export class apivideo extends plugin {
   constructor() {
     super({
       name: '反击API',
@@ -50,91 +48,71 @@ export class apiviedo extends plugin {
           reg: /^#?随机(小姐姐)(视频)?$/i,
           fnc: 'xjj',
         },
-        /* 		{
-                  reg: `^#?(${originalValues.join('|')})$`,
-                  fnc: 'jh'
-                }, */
         {
           reg: '^#?随机慢摇视频$',
           fnc: 'manyao'
-        },
-        {
-          reg: /^#?查看全部(随机)?视频(类型)?/,
-          fnc: 'ck'
         }
       ],
     });
   }
+
   async manyao(e) {
-    let aw = await this.ffmpeg()
-    if (aw) { return }
-    await this.viedo(e, 'http://api.yujn.cn/api/manyao.php', path.join(__dirname, '../resource/manyaoviedo'))
-  }
-  async ck(e) {
-    e.reply(originalValues.join('\n'))
+    await this.requestVideo(e, 'http://api.yujn.cn/api/manyao.php', path.join(__dirname, '../resource/manyaovideo'));
   }
   async hs(e) {
-    let aw = await this.ffmpeg()
-    if (aw) { return }
-    await this.viedo(e, apiurl, path.join(__dirname, '../resource/hsviedo'))
+    await this.requestVideo(e, apiurl, path.join(__dirname, '../resource/hsvideo'));
   }
-  async bs(e) {
-    let aw = await this.ffmpeg()
-    if (aw) { return }
-    await this.viedo(e, apiurl2, path.join(__dirname, '../resource/bsviedo'))
-  }
-  async xjj(e) {
-    let aw = await this.ffmpeg()
-    if (aw) { return }
-    await this.viedo(e, 'https://api.yunxiyuanyxy.xyz/plus/?type=302', path.join(__dirname, '../resource/xjjviedo'))
-  }
-  /* async jh (e) {
-      let aw = await this.ffmpeg();
-      if (!aw) { return; }
-      try {
-          let name = correspondingValues[originalValues.indexOf(e.msg.replace('#', ''))];
-          let urls = `http://api.hanhanz.gq:4006?category=${name}`;
-          let resp = await fetch(urls);
-          console.log(resp.url);
-          const absolutePath = path.join(__dirname, `../resource/${name}`);
-          await this.viedo(e, urls, absolutePath);
-      } catch (error) {
-          e.reply('报错：' + error);
-      }
-  } */
 
-  async viedo(e, apiUrl, defaultSavePath) {
+  async bs(e) {
+    await this.requestVideo(e, apiurl2, path.join(__dirname, '../resource/bsvideo'));
+  }
+
+  async xjj(e) {
+    await this.requestVideo(e, 'https://api.yunxiyuanyxy.xyz/plus/?type=302', path.join(__dirname, '../resource/xjjvideo'));
+  }
+
+  /**
+   * 请求视频数据，并处理保存和发送
+   * @param {Object} e - e
+   * @param {string} apiUrl - 视频URL
+   * @param {string} defaultSavePath - 默认保存路径
+   * @param {boolean} deleteAfterSend - 发送后是否删除文件
+   */
+  async requestVideo(e, apiUrl, defaultSavePath, deleteAfterSend = false) {
     try {
       const response = await fetch(apiUrl);
-
       if (!response.ok) {
-        throw new Error(`Failed to fetch video from ${apiUrl}`);
+        throw new Error(`[Fanji-plugin][api视频类] 从 ${apiUrl} 获取视频失败`);
       }
-
       const videoData = await response.buffer();
       const savePath = defaultSavePath || path.join(__dirname, '../resource/default');
       const timestamp = Date.now();
       const videoPath = path.join(savePath, `${timestamp}.mp4`);
-
-      // Ensure the directory exists before writing the file
       await fs.promises.mkdir(savePath, { recursive: true });
-
       await fs.promises.writeFile(videoPath, videoData);
       await e.reply([segment.video(videoPath)]);
-      await e.reply('From Fanji-plugin');
+      if (deleteAfterSend) {
+        await fs.promises.unlink(videoPath);
+      }
+      logger.info(`[Fanji-plugin][api视频类]成功从 ${apiUrl} 获取并发送视频`);
     } catch (error) {
-      console.error(`Error in viedo function: ${error.message}`);
+      logger.error(`[Fanji-plugin][api视频类] requestVideo 函数出错: ${error.message}`);
     }
   }
 
+  /**
+   * 检查是否安装了 ffmpeg
+   * @returns {boolean} - 返回 true为未安装 ffmpeg 否则返回 false
+   */
   async ffmpeg() {
-    let ret = await execSync('git -h', { encoding: 'utf-8' })
-    if (!ret || !ret.includes('version')) {
-      await this.reply('请先安装ffmpeg')
-      return true
+    try {
+      const ret = execSync('ffmpeg -version').toString();
+      if (!ret.includes('version')) {
+        throw new Error('未安装 ffmpeg');
+      }
+      return false;
+    } catch (error) {
+      return true;
     }
-    return false
   }
-
 }
-
