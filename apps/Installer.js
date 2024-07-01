1278f08c503de0c0d35b69d26b4a223e88ba3023
Admilk <admin@admilk.top>
2024-07-01 13:18:53 +0000

    修复权限验证
    Signed-off-by: Admilk <admin@admilk.top>

--- apps/Installer.js
+++ apps/Installer.js
@@ -111,7 +111,7 @@ export class PluginInstall extends plugin {
   }
 
   async install(e) {
-    if (!(e.isMaster || cm.check(e.user_id))) return await e.reply('你没有权限');
+    if (!(e.isMaster || await cm.check(e.user_id))) return await e.reply('你没有权限');
     // cm.tj().catch()
     if (isInstalling) {
       await e.reply("已有命令安装中，请勿重复操作");
