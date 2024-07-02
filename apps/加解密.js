f93302656957d01f3778a0dd6f2bd776362b1f51
Admilk <13205155+adrae@user.noreply.gitee.com>
2024-07-02 14:55:58 +0800

    1

--- apps/加解密.js
+++ apps/加解密.js
@@ -30,7 +30,7 @@ export class example extends plugin {
           fnc: 'jm'
         },
         {
-          reg: '^#?反击(加密|解密|转换)帮助$',
+          reg: '^#?反击(加密|转换|转化)帮助$',
           fnc: 'help'
         }
       ]
@@ -41,7 +41,7 @@ export class example extends plugin {
     let keys = Object.keys(operationsMap).forEach(key=>msg.push(key))
     msg.push('使用#操作名字 转换 文字 即可转换\n示例：#base64转换123\n#md5 base64转换123')
     msg.push('操作是从头开始按顺序执行的,如\n#md5 base64转换123的操作是\n先md5123然后把得到的值base64')
-    await e.reply(await Bot.makeForwardMsg(msg))
+    await e.reply(await Bot.makeForwardMsg([msg]))
   }
   async jm(e) {
     const commandPattern = new RegExp(`^#?((?:\\s*(?:${Object.keys(operationsMap).join('|')})\\s*)+)(转换|加密|转化)`, 'i');
