14e88c8ccbb78290389f5a0a0b0c8518796e734c
叶殇 <197728340@qq.com>
2024-07-18 05:10:07 +0000

    细节优化
    Signed-off-by: 叶殇 <197728340@qq.com>

--- guoba.support.js
+++ guoba.support.js
@@ -24,7 +24,7 @@ export function supportGuoba() {
       description: '主要提供清凉图api功能',
       // 显示图标，此为个性化配置
       // 图标可在 https://icon-sets.iconify.design 这里进行搜索
-      icon: 'mdi:stove',
+      icon: 'svg-spinners:blocks-scale',
       // 图标颜色，例：#FF0000 或 rgb(255, 0, 0)
       iconColor: '#d19f56',
       // 如果想要显示成图片，也可以填写图标路径（绝对路径）
